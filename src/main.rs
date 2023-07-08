55ac52aad29732a7845637ff34ba75a7010ff8b3
Ed Sweeney <ed@onextent.com>
2023-07-08 14:48:46 -0700

    working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use k8s_openapi::api::core::v1::Pod;
+use k8s_openapi::apimachinery::pkg::util::intstr::IntOrString;
 use kube::api::ObjectList;
 use kube::{
     api::{Api, ListParams},
@@ -9,9 +10,16 @@ use kube::{
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
+    /// Name of the namespace to walk
     #[arg(short, long)]
     namespace: String,
+}
+
+fn get_port(p: IntOrString) -> String {
+    match p {
+        k8s_openapi::apimachinery::pkg::util::intstr::IntOrString::Int(i) => i.to_string(),
+        k8s_openapi::apimachinery::pkg::util::intstr::IntOrString::String(s) => s,
+    }
 }
 
 #[tokio::main]
@@ -30,34 +38,22 @@ async fn main() -> Result<(), kube::Error> {
         for container in p.spec.unwrap().containers {
             if let Some(readiness_probe) = container.readiness_probe {
                 if let Some(http_get) = readiness_probe.http_get {
-                    let port = match http_get.port {
-                        k8s_openapi::apimachinery::pkg::util::intstr::IntOrString::Int(i) => {
-                            i.to_string()
-                        }
-                        k8s_openapi::apimachinery::pkg::util::intstr::IntOrString::String(s) => s,
-                    };
                     println!(
                         "Readiness Probe - Pod: {}, Path: {}, Port: {}",
                         metadata_name,
                         http_get.path.unwrap(),
-                        port
+                        get_port(http_get.port)
                     );
                 }
             }
 
             if let Some(liveness_probe) = container.liveness_probe {
                 if let Some(http_get) = liveness_probe.http_get {
-                    let port = match http_get.port {
-                        k8s_openapi::apimachinery::pkg::util::intstr::IntOrString::Int(i) => {
-                            i.to_string()
-                        }
-                        k8s_openapi::apimachinery::pkg::util::intstr::IntOrString::String(s) => s,
-                    };
                     println!(
                         "Liveness Probe - Pod: {}, Path: {}, Port: {}",
                         metadata_name,
                         http_get.path.unwrap(),
-                        port
+                        get_port(http_get.port)
                     );
                 }
             }

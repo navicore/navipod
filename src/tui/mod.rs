356f3bc33e6047a0e98cbafaee55c5fb00a9df52
Ed Sweeney <ed@onextent.com>
2024-01-28 07:42:42 -0800

    async key collection works

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,3 +1,10 @@
+use crossterm::event::{poll, read};
+use futures::stream::Stream;
+use futures::stream::StreamExt; // Needed for the `.next()` method
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::time::Duration;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream; // Assuming you're using crossterm for events
 mod container_app;
 use std::sync::Arc;
 pub mod data;
@@ -5,18 +12,17 @@ mod pod_app;
 mod rs_app;
 mod style;
 mod table_ui;
-
-use std::{error::Error, io};
-
 use crate::k8s::pods::list_rspods;
 use crate::k8s::rs::list_replicas;
 use crate::tui::table_ui::TuiTableState;
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    event::{DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ratatui::prelude::*;
+use std::{error::Error, io};
+use tracing::error;
 
 /// # Errors
 ///
@@ -41,7 +47,7 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
     terminal.show_cursor()?;
 
     if let Err(err) = res {
-        println!("{err:?}");
+        error!("{err:?}");
     }
 
     Ok(())
@@ -55,6 +61,8 @@ enum Apps {
 }
 
 async fn run_app<B: Backend + Send>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    let should_stop = Arc::new(AtomicBool::new(false));
+    let mut key_events = async_key_events(should_stop.clone());
     let data_vec = match list_replicas().await {
         Ok(d) => d,
         Err(e) => return Err(io::Error::new(io::ErrorKind::Other, e.to_string())),
@@ -64,16 +72,19 @@ async fn run_app<B: Backend + Send>(terminal: &mut Terminal<B>) -> io::Result<()
     };
 
     let mut history: Vec<Arc<Apps>> = Vec::new();
-    //let mut history: Vec<Rc<Apps>> = Vec::new();
     loop {
         match &mut app_holder {
             Apps::Rs { app: rs_app } => {
                 terminal.draw(|f| rs_app::ui::ui(f, &mut rs_app.clone()))?;
-                if let Event::Key(key) = event::read()? {
+
+                if let Some(Event::Key(key)) = key_events.next().await {
                     if key.kind == KeyEventKind::Press {
                         use KeyCode::{Char, Down, Enter, Up};
                         match key.code {
-                            Char('q') => return Ok(()),
+                            Char('q') => {
+                                should_stop.store(true, Ordering::Relaxed);
+                                return Ok(());
+                            }
                             Char('j') | Down => rs_app.next(),
                             Char('k') | Up => rs_app.previous(),
                             Char('c' | 'C') => rs_app.next_color(),
@@ -104,11 +115,15 @@ async fn run_app<B: Backend + Send>(terminal: &mut Terminal<B>) -> io::Result<()
             }
             Apps::Pod { app: pod_app } => {
                 terminal.draw(|f| pod_app::ui::ui(f, &mut pod_app.clone()))?;
-                if let Event::Key(key) = event::read()? {
+
+                if let Some(Event::Key(key)) = key_events.next().await {
                     if key.kind == KeyEventKind::Press {
                         use KeyCode::{Char, Down, Enter, Esc, Up};
                         match key.code {
-                            Char('q') => return Ok(()),
+                            Char('q') => {
+                                should_stop.store(true, Ordering::Relaxed);
+                                return Ok(());
+                            }
                             Char('j') | Down => pod_app.next(),
                             Char('k') | Up => pod_app.previous(),
                             Char('c' | 'C') => pod_app.next_color(),
@@ -134,11 +149,15 @@ async fn run_app<B: Backend + Send>(terminal: &mut Terminal<B>) -> io::Result<()
             }
             Apps::Container { app: container_app } => {
                 terminal.draw(|f| container_app::ui::ui(f, &mut container_app.clone()))?;
-                if let Event::Key(key) = event::read()? {
+
+                if let Some(Event::Key(key)) = key_events.next().await {
                     if key.kind == KeyEventKind::Press {
                         use KeyCode::{Char, Down, Esc, Up};
                         match key.code {
-                            Char('q') => return Ok(()),
+                            Char('q') => {
+                                should_stop.store(true, Ordering::Relaxed);
+                                return Ok(());
+                            }
                             Char('j') | Down => container_app.next(),
                             Char('k') | Up => container_app.previous(),
                             Char('c' | 'C') => container_app.next_color(),
@@ -154,4 +173,33 @@ async fn run_app<B: Backend + Send>(terminal: &mut Terminal<B>) -> io::Result<()
             }
         }
     }
+}
+
+fn async_key_events(should_stop: Arc<AtomicBool>) -> impl Stream<Item = Event> {
+    let (tx, rx) = mpsc::channel(100); // `100` is the capacity of the channel
+
+    tokio::spawn(async move {
+        while !should_stop.load(Ordering::Relaxed) {
+            match poll(Duration::from_millis(100)) {
+                Ok(true) => {
+                    if let Ok(event) = read() {
+                        if tx.send(event).await.is_err() {
+                            error!("Error sending event");
+                            break;
+                        }
+                    }
+                }
+                Ok(false) => {
+                    // No event, continue the loop to check should_stop again
+                }
+                Err(e) => {
+                    error!("Error polling for events: {e}");
+                    break;
+                }
+            }
+            // The loop will also check the should_stop flag here
+        }
+    });
+
+    ReceiverStream::new(rx)
 }
